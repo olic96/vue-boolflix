0a874105c26b748eb0aa8adba5e1c23b8a6ea123
olic96 <francoolich@gmail.com>
2022-06-09 14:55:49 +0200

    scaf

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="app">
+    <BaseHeader/>
+    <BaseMain/>
+  </div>
+</template>
+
+<script>
+import BaseHeader from './components/blocks/BaseHeader.vue'
+import BaseMain from './components/blocks/BaseMain.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    BaseHeader,
+    BaseMain,   
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/blocks/BaseHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <form class="example">
+        <input type="text" placeholder="Search.." name="search">
+        <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
+  </header>
+</template>
+
+<script>
+export default {
+    name:'BaseHeader',
+}
+</script>
+
+<style>
+
+</style>
