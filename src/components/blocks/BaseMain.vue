029a738eb66025c82ce106a3b2910d6084aea100
olic96 <francoolich@gmail.com>
2022-06-10 16:06:15 +0200

    M1

--- src/components/blocks/BaseMain.vue
+++ src/components/blocks/BaseMain.vue
@@ -1,12 +1,33 @@
 <template>
   <main>
+    <ul>
+      <li v-for="movie in share.movies" :key="movie.id">
+        <h3>{{movie.title}}</h3>
+        <h4>{{movie.original_title}}</h4>
+        <p>{{movie.vote_average}}</p>
+      </li>
+    </ul>
 
-  </main>
+     <ul>
+      <li v-for="serie in share.series" :key="serie.id">
+        <h3>{{serie.name}}</h3>
+        <h4>{{serie.original_title}}</h4>
+        <p>{{serie.vote_average}}</p>
+      </li>
+    </ul>
+  </main> 
 </template>
 
 <script>
+import share from '../../shared/share';
+
 export default {
-    name:'BaseMain',
+  name: 'BaseMain',
+  data() {
+    return {
+      share,
+    }
+  }
 }
 </script>
 
