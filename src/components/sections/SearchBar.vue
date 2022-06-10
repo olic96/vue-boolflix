af8d70be4b7fa672794ae4e0d63238d559630703
olic96 <francoolich@gmail.com>
2022-06-10 12:44:01 +0200

    share

--- src/components/sections/SearchBar.vue
+++ src/components/sections/SearchBar.vue
@@ -1,24 +1,26 @@
 <template>
-  <form class="searchbar" @submit.prevent="submitForm()">
-        <input type="text" placeholder="Search.." name="search" v-model="search">
+  <form class="searchbar" @submit.prevent="searching()">
+        <input type="text" placeholder="Search.." name="search" v-model="search" required>
         <button type="submit"><i class="fa fa-search"></i></button>
     </form>
 </template>
 
 <script>
 import axios from 'axios';
+import share from '../../shared/share';
 
 export default {
     name:'SearchBar',
         data() {
             return {
+                share,
                 search:'',
             }
         },
 
         methods: {
             // film
-        submitForm() {
+        searching() {
             axios.get('https://api.themoviedb.org/3/search/movie', {
                 params: {
                     api_key:'2fcc653be32b6847d9dbdbc388549bfc',
@@ -26,12 +28,12 @@ export default {
                     language:'it-IT',
                 }
             }).then((response) => {
-                console.log(response.data.results);
+                share.movies = response.data.results;
             }).catch((error) => {
                 console.log(error);
             })
 
-            // serir tv
+            // serie tv
             axios.get('https://api.themoviedb.org/3/search/tv', {
                 params: {
                     api_key:'2fcc653be32b6847d9dbdbc388549bfc',
@@ -39,13 +41,11 @@ export default {
                     language:'it-IT',
                 }
             }).then((response) => {
-                console.log(response.data.results);
+                share.series = response.data.results;
             }).catch((error) => {
                 console.log(error);
             })
         },
-
-        
     }
 }
 </script>
