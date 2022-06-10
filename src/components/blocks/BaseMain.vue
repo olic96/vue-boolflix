2eafe7224102996dcf6b0feb58e191789a178a4e
olic96 <francoolich@gmail.com>
2022-06-10 17:10:31 +0200

    error

--- src/components/blocks/BaseMain.vue
+++ src/components/blocks/BaseMain.vue
@@ -28,24 +28,24 @@ export default {
   data() {
     return {
       share,
-      flag: {
-        it:'https://it.wikipedia.org/wiki/Bandiera_d%27Italia#/media/File:Flag_of_Italy.svg',
-        en:'https://it.wikipedia.org/wiki/Bandiera_del_Regno_Unito#/media/File:Flag_of_the_United_Kingdom_(3-5).svg',
-        es:'https://it.wikipedia.org/wiki/Bandiera_della_Spagna#/media/File:Flag_of_Spain.svg',
-        de:'https://it.wikipedia.org/wiki/Bandiera_della_Germania#/media/File:Flag_of_Germany.svg',
-        fr:'https://it.wikipedia.org/wiki/Bandiera_della_Francia#/media/File:Flag_of_France_(1794–1815,_1830–1974,_2020–present).svg',
-        globe:'https://it.wikipedia.org/wiki/Bandiera_della_Terra#/media/File:Earth_Day_Flag.png',
-      }
+      // flag: {
+      //   it:'https://it.wikipedia.org/wiki/Bandiera_d%27Italia#/media/File:Flag_of_Italy.svg',
+      //   en:'https://it.wikipedia.org/wiki/Bandiera_del_Regno_Unito#/media/File:Flag_of_the_United_Kingdom_(3-5).svg',
+      //   es:'https://it.wikipedia.org/wiki/Bandiera_della_Spagna#/media/File:Flag_of_Spain.svg',
+      //   de:'https://it.wikipedia.org/wiki/Bandiera_della_Germania#/media/File:Flag_of_Germany.svg',
+      //   fr:'https://it.wikipedia.org/wiki/Bandiera_della_Francia#/media/File:Flag_of_France_(1794–1815,_1830–1974,_2020–present).svg',
+      //   globe:'https://it.wikipedia.org/wiki/Bandiera_della_Terra#/media/File:Earth_Day_Flag.png',
+      // }
     }
   },
 
-  methods: {
-    getFlag(original_language) {
-      if(original_language === 'en') {
-        return this.data.flag.en;
-      }
-    }
-  }
+  // methods: {
+  //   getFlag(lang) {
+  //     if(lang === 'en') {
+  //       return this.data.flag.en;
+  //     }
+  //   }
+  // }
 }
 </script>
 
---
+++ src/components/commons/FilmCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <ul>
+        <li v-for="movie in movies" :key="movie.id">
+        <img :src="movie.poster_path" :alt="film.title">
+        {{movie.title}}<br>
+        {{movie.original_title}}
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+    name:'FilmCard',
+    props: {
+        movies: Array,
+        series: Array
+    },
+}
+</script>
+
+<style>
+
+</style>
